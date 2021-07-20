62e8e8950d88bff44dab343a2abd86ad3db8620a
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-20 11:41:02 +0100

    Create tasks list and single task components and mount to html with sample data

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 const app = Vue.createApp({
     data() {
-        return {}
+        return {
+            counter: 0,
+            tasks: [
+                "first task",
+                "second task"
+            ]
+        }
     }
 });
 
+app.component('tasks-list', {
+    props: {
+        tasks: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div class="container-sm">
+            <single-task v-for="(task, index) in tasks" :task="task" :key="index">
+            </single-task>
+    `
+});
 
+app.component('single-task', {
+    props: {
+        task: {
+            type: String,
+            required: true
+        }
+    },
+    template: `
+        <div class="alert alert-success alert-dismissible fade show my-1" role="alert">
+            {{ task }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `
+})
 
 app.mount('#app');
